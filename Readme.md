d2fc52898eae3174a50696b26a8bc4ee9d486d29
Clayton Burlison <github@clburlison.com>
2016-05-31 23:52:56 -0500

    add pics, packages, and script

--- Readme.md
+++ Readme.md
@@ -2,3 +2,9 @@ imagr-testing
 ===
 
 Files that can help with testing Imagr related things...
+
+
+
+# Alternatives
+
+`https://github.com/munki/contrib/blob/master/munki_kickstart.pkg?raw=true`
---
+++ scripts/dummy_say.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+%{ color=red } "Is this working. Lets sleep now."
+%{ color=green } "More text is here..."
+/bin/sleep 10
+/usr/bin/say "Very important words are placed here"
+
+%{ color=blue } "Goodbye!"
+
+exit 0
